324c1b6a5e70dea877cb64ebca29365a6e6126d4
pixelynx <tjstevens1992@gmail.com>
2019-03-12 15:23:02 -0400

    env setup

---
+++ frontend/src/components/homepage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+class Homepage extends Component {
+  render() {
+    return(
+      <>
+      <div>Hello</div>
+      </>
+    )
+  }
+}
+
+export default Homepage;
